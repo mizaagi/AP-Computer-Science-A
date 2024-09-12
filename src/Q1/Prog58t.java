da6b441358c8c0a20e69d36f783e9e033e432f13
kohn.m <marlon.c.kohn@gmail.com>
2024-09-12 11:23:08 -0500

    9/12/2024

--- src/Q1/Prog58t.java
+++ src/Q1/Prog58t.java
@@ -1,7 +1,25 @@
 package Q1;
+import java.util.*;
 
 public class Prog58t {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter purchase price: ");
+        double pprice = input.nextDouble();
+        System.out.println("Enter amount received: ");
+        double received = input.nextDouble();
+        double changeTotal = Math.round((received - pprice) * 100.0) / 100.0;
+        System.out.println(changeTotal);
+
+
+
+        System.out.println("Dollars: " + dollars);
+        System.out.println("Quarters: " + quarters);
+        System.out.println("Dimes: " + dimes);
+        System.out.println("Nickels: " + nickels);
+        System.out.println("Pennies: " + pennies);
+
 
     }
 }
