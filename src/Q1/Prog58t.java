13843d2d0e2d5e0444556a59638ff45d804da6bf
kohn.m <marlon.c.kohn@gmail.com>
2024-09-13 11:09:09 -0500

    9/13/2024

--- src/Q1/Prog58t.java
+++ src/Q1/Prog58t.java
@@ -12,6 +12,17 @@ public class Prog58t {
         double changeTotal = Math.round((received - pprice) * 100.0) / 100.0;
         System.out.println(changeTotal);
 
+        int dollars = 0;
+        int quarters = 0;
+        int dimes = 0;
+        int nickels = 0;
+        int pennies = 0;
+
+        while (changeTotal >= 1)    { dollars += 1; changeTotal -= 1; }
+        while (changeTotal >= 0.25) { quarters += 1; changeTotal -= 0.25; }
+        while (changeTotal >= 0.10) { dimes += 1; changeTotal -= 0.10; }
+        while (changeTotal >= 0.05) { nickels += 1; changeTotal -= 0.05; }
+        while (changeTotal >= 0.01) { pennies += 1; changeTotal -= 0.01; }
 
 
         System.out.println("Dollars: " + dollars);
