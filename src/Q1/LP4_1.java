cedd8a1c6954bc5386ca6a7dbf22bb77fa02bafc
kohn.m <marlon.c.kohn@gmail.com>
2024-09-27 11:23:43 -0500

    9/27/2024

---
+++ src/Q1/LP4_1.java
@@ -0,0 +1,12 @@
+package Q1;
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Scanner;
+
+public class LP4_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number of copies: ");
+        int copies = input.nextInt();
+    }
+}
