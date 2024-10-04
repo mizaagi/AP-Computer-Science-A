27cb448a919d87a89d4b7e8b0c2ada7d75d2ef6d
kohn.m <marlon.c.kohn@gmail.com>
2024-10-04 10:13:56 -0500

    10/4/2024

--- src/Q1/LP4_2.java
+++ src/Q1/LP4_2.java
@@ -1,4 +1,32 @@
 package Q1;
+import java.util.*;
 
 public class LP4_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter weight in kg: ");
+        double weight;
+        weight = input.nextDouble();
+        System.out.println("Enter length in cm: ");
+        double length;
+        length = input.nextDouble();
+        System.out.println("Enter width in cm: ");
+        double width;
+        width = input.nextDouble();
+        System.out.println("Enter height in cm: ");
+        double height;
+        height = input.nextDouble();
+        double volume = length * width * height;
+
+        if (volume > 100000 && weight > 27) {
+            System.out.println("Package is too heavy and too large.");
+        } else if (volume > 100000) {
+            System.out.println("Package is too large.");
+        } else if (weight > 27) {
+            System.out.println("Package is too heavy.");
+        } else {
+            System.out.println("Success!");
+        }
+
+    }
 }
--- src/Q1/LP4_3.java
+++ src/Q1/LP4_3.java
@@ -1,4 +1,32 @@
 package Q1;
+import java.util.*;
+
 
 public class LP4_3 {
+    public static double getTotal(double x, int dozens, int spares) {
+        double newTotal = (dozens * x) + (spares * (x / 12));
+        System.out.println("The total price is $" + newTotal);
+        return newTotal;
+    }
+
+    public static void main(String[] args) {
+        int eggs = 0;
+        System.out.println("Enter the number of eggs you are purchasing: ");
+        Scanner input = new Scanner(System.in);
+        eggs = input.nextInt();
+        int dozens = eggs / 12;
+        int spares = eggs % 12;
+        double price = 0.0;
+        double total = 0.0;
+
+        if (eggs >= 0 && eggs < 48) {
+            getTotal(0.5, dozens, spares);
+        } else if (eggs >= 48 && eggs < 72) {
+            getTotal(0.45, dozens, spares);
+        } else if (eggs >= 72 && eggs < 132) {
+            getTotal(0.4, dozens, spares);
+        } else if (eggs >= 132) {
+            getTotal(0.35, dozens, spares);
+        }
+    }
 }
