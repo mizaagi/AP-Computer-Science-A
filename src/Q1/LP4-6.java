60463d062adbf3e16e78ad66041d47619a2d02b1
mizaagi <126592834+mizaagi@users.noreply.github.com>
2024-10-16 22:49:44 +0000

    ???

---
+++ src/Q1/LP4-6.java
@@ -0,0 +1,11 @@
+package Q1;
+import java.util.*;
+
+public class LP4_6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1 = Math.ceil(Math.random()) * 10;
+        int num2 = Math.ceil(Math.random()) * 10;
+        int sign = 
+    }
+}
