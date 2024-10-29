8ff29ac71750f374c5e2c25d960db14409809150
kohn.m <marlon.c.kohn@gmail.com>
2024-10-29 11:19:33 -0500

    10/17/2024

---
+++ src/Q1/Prog213bClass/Class213b.java
@@ -0,0 +1,15 @@
+package Q1.Prog213bClass;
+
+public class Class213b {
+    private int myQuantity;
+    private double myPrice;
+    private double myAmtDue;
+
+    public Cl213b(int quantity) {
+        myQuantity = quantity;
+        myPrice = 0;
+        myAmtDue = 0;
+    }
+
+    // Please help me get answers from other people :( I'm too busy bro
+}
