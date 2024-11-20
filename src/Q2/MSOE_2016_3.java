fd03983f3c478cc5e0aeccf017215236a456aebe
mizaagi <126592834+mizaagi@users.noreply.github.com>
2024-11-20 23:14:47 +0000

    11/20/2024

--- src/Q2/MSOE_2016_3.java
+++ src/Q2/MSOE_2016_3.java
@@ -1,7 +1,14 @@
 package Q2;
+import java.util.Scanner;
 
 public class MSOE_2016_3 {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        double n = input.nextDouble();
+        double r1 = input.nextDouble();
+        double r2 = input.nextDouble();
+        double d = input.nextDouble();
+        double f = (n - 1) * ((1/r1) - (1/r2) + (((n - 1) * d) / (n * r1 * r2)));
+        System.out.println("1/f is equal to " + f + ".");
     }
 }
