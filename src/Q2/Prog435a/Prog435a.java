24427b05321b3c8cda847ce265b504ddeb29b754
kohn.m <marlon.c.kohn@gmail.com>
2024-11-21 10:50:39 -0600

    11/21/2024

--- src/Q2/Prog435a/Prog435a.java
+++ src/Q2/Prog435a/Prog435a.java
@@ -8,14 +8,18 @@ public class Prog435a {
     public static void main(String[] args) {
         try {
             Scanner file = new Scanner(new File("Langdat/prog435a.dat"));
-
+            Cl435a[] cars = new Cl435a[18];
+            int count = 0;
             while (file.hasNext()) {
                 int vType = file.nextInt();
                 int gate  = file.nextInt();
                 Cl435a car = new Cl435a(vType, gate);
                 car.calc();
-                System.out.println(car);
+                cars[count] = car;
+                count++;
             }
+            for (Cl435a car : cars)
+                System.out.println(car);
         } catch (IOException e) {
             System.out.println("Error: " + e);
         }
