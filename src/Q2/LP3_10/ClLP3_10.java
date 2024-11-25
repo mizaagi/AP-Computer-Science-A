03342a4669b20b43b84a7a882e1fd4f4b1796e4b
kohn.m <marlon.c.kohn@gmail.com>
2024-11-25 11:24:15 -0600

    11/25/2024

---
+++ src/Q2/LP3_10/ClLP3_10.java
@@ -0,0 +1,10 @@
+package Q2.LP3_10;
+
+public class ClLP3_10 {
+    private int burgers;
+    private int fries;
+    private int sodas;
+    private double subtot;
+    private double tax;
+    private double fintax;
+}
---
+++ src/Q2/LP3_10/LP3_10.java
@@ -0,0 +1,4 @@
+package Q2.LP3_10;
+
+public class LP3_10 {
+}
