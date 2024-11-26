7c13afa50fb690acf04ec4d9196e47f2d6337c7c
mizaagi <126592834+mizaagi@users.noreply.github.com>
2024-11-26 00:44:49 +0000

    11/25/24

--- src/Q2/LP3_10/ClLP3_10.java
+++ src/Q2/LP3_10/ClLP3_10.java
@@ -1,4 +1,5 @@
 package Q2.LP3_10;
+import java.util.Scanner;
 
 public class ClLP3_10 {
     private int burgers;
@@ -6,5 +7,35 @@ public class ClLP3_10 {
     private int sodas;
     private double subtot;
     private double tax;
-    private double fintax;
+    private double fintot;
+    private double tender;
+    private double change;
+
+    public ClLP3_10(int b, int f, int s) {
+        this.burgers = b;
+        this.fries   = f;
+        this.sodas   = s;
+        this.subtot  = 0;
+        this.tax     = 0;
+        this.fintot  = 0;
+        this.change  = 0;
+    }
+
+    public void calc() {
+        this.subtot = (burgers * 1.69) + (fries * 1.09) + (sodas * 0.99);
+        this.tax    = subtot * 0.065;
+        this.fintot = subtot + tax;
+    }
+
+    public void calc_tender(double t, double p) {
+        this.change = t - p;
+    }
+
+    public String toString() {
+        return "\nSubtotal: $" + subtot + "\nTax: $" + tax + "\nTotal: $" + fintot + "\n";
+    }
+
+    public String getChangeString() {
+        return "Your change is " + change;
+    } 
 }
--- src/Q2/LP3_10/LP3_10.java
+++ src/Q2/LP3_10/LP3_10.java
@@ -1,4 +1,24 @@
 package Q2.LP3_10;
+import java.util.Scanner;
 
 public class LP3_10 {
+    public static void main(String[] args) {
+        int b, f, s;
+        double t;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of burgers: ");
+        b = input.nextInt();
+        System.out.print("Enter the number of fries: ");
+        f = input.nextInt();
+        System.out.print("Enter the number of sodas: ");
+        s = input.nextInt();
+
+        ClLP3_10 transaction = new ClLP3_10(b, f, s);
+        transaction.calc();
+        System.out.println(transaction);
+        System.out.print("Enter amount tendered: ");
+        t = input.nextDouble();
+        transaction.calc_tender(t);
+        System.out.println(transaction.getChangeString());
+    }
 }
