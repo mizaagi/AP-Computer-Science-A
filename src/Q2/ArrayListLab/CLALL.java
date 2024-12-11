2164995e50b87cf17c04ea380048bcc56865a4bb
kohn.m <marlon.c.kohn@gmail.com>
2024-12-11 11:24:51 -0600

    12/09/2024

--- src/Q2/ArrayListLab/CLALL.java
+++ src/Q2/ArrayListLab/CLALL.java
@@ -1,4 +1,14 @@
 package Q2.ArrayListLab;
 
 public class CLALL {
+    private int[] myArray;
+    private int mySize;
+    private int myCapacity;
+    private final int DEFAULT_SIZE = 16;
+
+    public CLALL() {
+        myArray = new int[DEFAULT_SIZE];
+        mySize = 0;
+        myCapacity = DEFAULT_SIZE;
+    }
 }
