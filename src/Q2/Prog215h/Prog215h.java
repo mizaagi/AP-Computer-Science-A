69f9806cba742c8ece4cff3118615ff568b8e2e3
kohn.m <marlon.c.kohn@gmail.com>
2024-12-18 11:27:56 -0600

    12/18/2024

--- src/Q2/Prog215h/Prog215h.java
+++ src/Q2/Prog215h/Prog215h.java
@@ -8,12 +8,26 @@ public class Prog215h {
     public static void main(String[] args) {
         try {
             Scanner file = new Scanner(new File("Langdat/prog215h.txt"));
-
+            Cl215h[] athletes = new Cl215h[6];
+            int count = 0;
             while (file.hasNext()) {
                 double[] scores = new double[8];
-                double s1, s2, s3, s4, s5, s6, s7, s8;
-                s1 = file.nextDouble();
+                scores[0] = file.nextDouble();
+                scores[1] = file.nextDouble();
+                scores[2] = file.nextDouble();
+                scores[3] = file.nextDouble();
+                scores[4] = file.nextDouble();
+                scores[5] = file.nextDouble();
+                scores[6] = file.nextDouble();
+                scores[7] = file.nextDouble();
+                Cl215h athlete = new Cl215h(count, scores);
+                athlete.calc();
+                athletes[count] = athlete;
+                count++;
             }
+            for (Cl215h a : athletes)
+                System.out.println(a);
+
         } catch (IOException e) {
             System.out.println("Error: " + e);
         }
