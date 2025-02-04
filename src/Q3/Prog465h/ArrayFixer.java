c03c1029c6e677890e4f0dc52ebff008209147e2
kohn.m <marlon.c.kohn@gmail.com>
2025-02-04 11:23:13 -0600

    2/4/2025

---
+++ src/Q3/Prog465h/ArrayFixer.java
@@ -0,0 +1,75 @@
+package Q3.Prog465h;
+
+public class ArrayFixer {
+    int rows;
+    int cols;
+    int[][] array;
+    int[][] newArray;
+
+    public ArrayFixer(int[][] arr, int r, int c) {
+        rows = r;
+        cols = c;
+        array = arr;
+        if (shouldBeReduced() >= 1)
+            newArray = new int[3][getReducedStats()];
+        else newArray = new int[0][0];
+    }
+
+    public int shouldBeReduced() {
+        int nonZeros = 0;
+        int zeros = 0;
+        for (int[] mat : array) {
+            for (int i : mat) {
+                if (i == 0) zeros++;
+                else nonZeros++;
+            }
+        }
+        // if it shouldn't be reduced, return 0
+        if (zeros < nonZeros) return 0;
+        // if it should be reduced, return 1
+        if (zeros > nonZeros) return 1;
+        // else return 2 if they are the same level of efficiency
+        return 2;
+    }
+
+    private int getReducedStats() {
+        int x = 0;
+        for (int[] mat : array)
+            for (int i : mat)
+                if (i != 0) x++;
+        return x;
+    }
+
+    public void calcReduced() {
+        int count = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (array[i][j] != 0 && count < cols) {
+                    newArray[count][0] = i;
+                    newArray[count][1] = j;
+                    newArray[count][2] = array[i][j];
+                    if (count < cols-1)
+                        count++;
+                }
+            }
+        }
+    }
+
+    public void printFormattedReduced() {
+        for (int[] mat : newArray) {
+            for (int i : mat) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printFormattedNormal() {
+        for (int[] mat : array) {
+            for (int i : mat) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Q3/Prog465h/Prog465h.java
@@ -0,0 +1,28 @@
+package Q3.Prog465h;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prog465h {
+    public static void main(String[] args) {
+        try {
+            Scanner file = new Scanner(new File("Langdat/prog465h.dat"));
+
+            while (file.hasNext()) {
+                int k = file.nextInt();
+                int l = file.nextInt();
+                int[][] mat = new int[k][l];
+                for (int i = 0; i < k; i++)
+                    for (int j = 0; j < l; j++)
+                        mat[i][j] = file.nextInt();
+                ArrayFixer hello = new ArrayFixer(mat, k, l);
+                hello.calcReduced();
+                if (hello.shouldBeReduced() >= 1) hello.printFormattedReduced();
+                else hello.printFormattedNormal();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+}
