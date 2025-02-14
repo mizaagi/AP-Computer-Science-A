311464dba3a5c6be3d49c1d2cef23243deee82b2
kohn.m <marlon.c.kohn@gmail.com>
2025-02-14 11:25:56 -0600

    2/14/2025

--- src/Q3/Prog607a/EncodeDecode.java
+++ src/Q3/Prog607a/EncodeDecode.java
@@ -1,4 +1,23 @@
 package Q3.Prog607a;
 
 public class EncodeDecode {
+    private String monthAlp;
+    private String dayAlp;
+    private String yearAlp;
+    private int day;
+    private int month;
+    private int year;
+
+    public EncodeDecode(String date) {
+        monthAlp = "abcdefghijkl";
+        dayAlp   = "qrstuvwxyz";
+        yearAlp  = "zyxwbutsrqponmlkjihgfedcba";
+        if (date.substring(0, 1).equals("0"))
+            day = Integer.parseInt(date.substring(0, 1));
+        else
+            day = Integer.parseInt(date.substring(0, 2));
+        month = Integer.parseInt(date.substring(3, 5));
+        year = Integer.parseInt(date.substring(6));
+        // 01/34/6789
+    }
 }
