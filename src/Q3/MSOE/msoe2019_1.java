a99413c1c02ad15760d4e70f33b59071578016ed
Marlon Kohn <126592834+mizaagi@users.noreply.github.com>
2025-04-07 11:22:30 -0500

    4/7/2025

--- src/Q3/MSOE/msoe2019_1.java
+++ src/Q3/MSOE/msoe2019_1.java
@@ -5,7 +5,15 @@ public class msoe2019_1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter first letter: ");
-
+        String c1 = input.nextLine();
         System.out.print("Enter second letter: ");
+        String c2 = input.nextLine();
+        System.out.println("Enter line of text: ");
+        String line = input.nextLine();
+        int count = 0;
+        for (int i = 0; i < line.length()-1; i++) {
+            if (line.substring(i, i+2).equals(c1 + c2) || line.substring(i, i+2).equals(c2 + c1)) count++;
+        }
+        System.out.println(count);
     }
 }
---
+++ src/Q3/MSOE/msoe2021_1.java
@@ -0,0 +1,13 @@
+package Q3.MSOE;
+
+import java.util.Scanner;
+
+public class msoe2021_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first letter: ");
+        String c1 = input.nextLine();
+        System.out.println("Enter line of text: ");
+        String line = input.nextLine();
+    }
+}
