46bcda8793ad253068ea097f4207f917def9dda5
Marlon Kohn <126592834+mizaagi@users.noreply.github.com>
2025-04-09 11:25:27 -0500

    4/9/2025

--- src/Q3/MSOE/msoe2021_1.java
+++ src/Q3/MSOE/msoe2021_1.java
@@ -1,5 +1,7 @@
 package Q3.MSOE;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.util.Scanner;
 
 public class msoe2021_1 {
@@ -9,5 +11,19 @@ public class msoe2021_1 {
         String c1 = input.nextLine();
         System.out.println("Enter line of text: ");
         String line = input.nextLine();
+        if (line.contains(c1)) {
+            int x = line.indexOf(" ");
+            while (!line.substring(x-1, x).equals(c1) && line.contains(" ")) {
+                line = line.substring(x + 1);
+                x = line.indexOf(" ");
+            }
+            if (line.contains(" "))
+                System.out.println(line.substring(0, line.indexOf(" ")));
+            else
+                System.out.println(line);
+        } else {
+            System.out.println("not found");
+        }
+
     }
 }
