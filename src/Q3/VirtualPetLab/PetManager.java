a81cb68685c51f22fe65c0a1261cb6d0435cedf8
Marlon Kohn <126592834+mizaagi@users.noreply.github.com>
2025-04-27 17:05:36 -0500

    Update PetManager.java

--- src/Q3/VirtualPetLab/PetManager.java
+++ src/Q3/VirtualPetLab/PetManager.java
@@ -1,20 +1,16 @@
-package Q3.VirtualPetLab;
+package Quarter4.VirtualPetLab;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class PetManager {
     private ArrayList<Pet> pets = new ArrayList<>();
 
-    // TODO: Implement methods as needed to interact with the pets list
-    public void addPet(Pet pet) {
-        pets.add(pet);
-    }
-
-    public void removePet(Pet pet) {
-        pets.remove(pet);
-    }
-
-    public ArrayList<Pet> getPets() {
-        return pets;
-    }
+    // Implement methods as needed to interact with the pets list
+    public void addPet(Pet pet) { pets.add(pet); }
+    public void removePet(Pet pet) { pets.remove(pet); }
+    public void listPets() { for (Pet pet : pets) System.out.print(pet.getName() + " "); }
+    public ArrayList<Pet> getListPets() { return pets; }
+    public int getNumPets() { return pets.size(); }
+    public Pet getSelectedPet(int lcv) { return pets.get(lcv); }
 
 }
