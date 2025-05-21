9d2434847942ee40eefb3c2ec74c0e08e670380d
Marlon Kohn <126592834+mizaagi@users.noreply.github.com>
2025-05-21 11:36:15 -0500

    5/21/2025

--- src/Q4/LibraryManagementLab/BinarySearchUtil.java
+++ src/Q4/LibraryManagementLab/BinarySearchUtil.java
@@ -4,6 +4,6 @@ public class BinarySearchUtil {
     // TODO: Implement binary search method (takes in ArrayList) using recursion for searching books by title (required) or author
     static int binarySearch(int a[], int l, int r, int x)
     {
-
+    
     }
 }
