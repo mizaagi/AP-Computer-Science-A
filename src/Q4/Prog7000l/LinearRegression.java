f5138f8184d2fae12d1de68a75af1ad6597b0ef3
Marlon Kohn <126592834+mizaagi@users.noreply.github.com>
2025-05-22 11:24:09 -0500

    5/22/2025

--- src/Q4/Prog7000l/LinearRegression.java
+++ src/Q4/Prog7000l/LinearRegression.java
@@ -51,5 +51,7 @@ public class LinearRegression {
     public double getIntercept()    { return theta0;              }
     public double getSlope()        { return theta1;              }
 
-    // TODO: toString()
+    public String toString() {
+        return "Regression Line: y = " + theta1 + " * x + " + theta0 + "\nObjective" + Arrays.toString(objective) + "\nMin. Objective " + Arrays.stream(objective).min().getAsDouble();
+    }
 }
